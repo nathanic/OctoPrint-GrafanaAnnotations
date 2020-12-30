3f97f33814b69babe75c1ed794b57e7d7fba9de5
Nathan Stien <nathanism@gmail.com>
2020-12-29 22:22:05 -0600

    initial working plugin

---
+++ octoprint_GrafanaAnnotations/__init__.py
@@ -0,0 +1,100 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+from octoprint.events import Events
+
+# requests lib seems to be part of base octoprint
+import requests
+import time
+from urllib.parse import urlparse
+
+TIMEOUT=5
+
+class GrafanaAnnotationPlugin(octoprint.plugin.EventHandlerPlugin,
+                              octoprint.plugin.SettingsPlugin):
+    def __init__(self):
+        self.currentAnnotationId = None
+
+    def get_settings_defaults(self):
+        return dict(url='https://mygrafana:3000', 
+                api_key='changeme',
+                dashboard_id='', 
+                panel_id='')
+
+    def on_event(self, event, payload):
+        self._logger.debug("Received event %s" % event)
+        if event == Events.PRINT_STARTED:
+            self.begin_print_annotation(payload)
+        elif event in [Events.PRINT_FAILED, Events.PRINT_CANCELLED]:
+            self.end_print_annotation(False, payload)
+        elif event == Events.PRINT_DONE:
+            self.end_print_annotation(True, payload)
+        # do we care about PRINT_PAUSED/RESUMED ?
+
+    def begin_print_annotation(self, payload):
+        # this bit was inspired by grafannotate
+        # https://github.com/devopsmakers/python-grafannotate
+        ann = {'text': 'Print of %s' % payload['path'],
+                'time': int(round(time.time() * 1000)),
+                'tags': ['printjob', self.get_printer_tag() ] 
+                }
+        self._logger.info('kilroy was here')
+        self._logger.info(self._settings.get(['url']))
+        if self._settings.get(['dashboard_id']) != '':
+            ann['dashboardId'] = self._settings.get(['dashboard_id']) 
+        if self._settings.get(['panel_id']) != '':
+            ann['panelId'] = self._settings.get(['panel_id']) 
+
+        self._logger.info('Creating Grafana Annotation with content %s' % (ann))
+        response = requests.post(self.get_api_url(), json=ann, headers=self.headers(), timeout=TIMEOUT)
+
+        if response.ok:
+            result = response.json()
+
+            if 'id' in result:
+                self.currentAnnotationId = result['id']
+            if result['message']:
+                self._logger.info(result['message'])
+            self._logger.info('Grafana Annotation has ID %s' % self.currentAnnotationId)
+        else:
+            self._logger.error('Failed posting to grafana with response %s' % response.json)
+
+    def end_print_annotation(self, success, payload):
+        if self.currentAnnotationId is None:
+            self._logger.warn("Can't update grafana annotation because we don't have a current ID")
+            return
+
+        end_time = time.time()
+        tags = ['printjob', self.get_printer_tag()]
+        if success:
+            tags.append('printjob_succeeded')
+        else:
+            tags.append('printjob_failed')
+        ann = { 'time': unixtime_to_javatime(end_time - payload['time']), 
+                'timeEnd': unixtime_to_javatime(end_time), 
+                'tags': tags }
+        self._logger.info('Patching Grafana Annotation ID %d with content %s' % (self.currentAnnotationId, ann))
+        url = '%s/%d' % (self.get_api_url(), self.currentAnnotationId) 
+        response = requests.patch(url, headers=self.headers(), json=ann, timeout=TIMEOUT)
+        self.currentAnnotationId = None
+        self._logger.info('Grafana Annotation API Response: %s' % response.json())
+
+    def headers(self):
+        return { 'Authorization': 'Bearer ' + self._settings.get(['api_key']),
+                 'Accept': 'application/json',
+                 'Content-Type': 'application/json'
+                }
+    def get_api_url(self):
+        return self._settings.get(['url']) + '/api/annotations'
+
+    def get_printer_tag(self):
+        # minimal effort
+        #return 'printer_' + self._settings.global_get(['appearance','name']).replace(' ', '_').lower()
+        return 'printer_test'
+
+def unixtime_to_javatime(t):
+    return int(round( t * 1000))
+
+__plugin_implementation__ = GrafanaAnnotationPlugin()
+__plugin_pythoncompat__ = ">=2.7,<4"
